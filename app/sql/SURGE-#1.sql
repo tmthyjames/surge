f7c4ec8505cedbec51956fa30029d5b827ddbbd2
Tim Dobbins <tmthyjames@gmail.com>
2017-11-04 13:30:33 -0500

    add db

--- app/sql/SURGE-#1.sql
+++ app/sql/SURGE-#1.sql
@@ -5,6 +5,8 @@
 -- Dumped from database version 9.6.2
 -- Dumped by pg_dump version 9.6.2
 
+CREATE DATABASE surge;
+
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
